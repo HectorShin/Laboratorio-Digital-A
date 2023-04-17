a9cb594309284e12a9ae6bc69ec870bc108b501e
massaotibana <97815238+massaotibana@users.noreply.github.com>
2023-04-17 14:55:59 -0300

    Update toplevel.vhd

--- Experiência-4/toplevel.vhd
+++ Experiência-4/toplevel.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 
 entity toplevel is
     port (
-        data:  in std_logic;
+        data : in std_logic;
         clock : in std_logic;
         reset : in std_logic;
         display1 : out std_logic_vector(7 downto 0);
@@ -17,57 +17,69 @@ entity toplevel is
 end entity;
 
 architecture topleve_arch of toplevel is
-    -- sinais
-        signal data_rx : std_logic_vector(8 downto 0);
-        signal paridade_rx : std_logic;
-        signal clock_projeto : std_logic;
-        signal data_out : std_logic_vector(47 downto 0);
-        signal data_out1 : std_logic_vector(7 downto 0);
-        signal data_out2 : std_logic_vector(7 downto 0);
-        signal data_out3 : std_logic_vector(7 downto 0);
-        signal data_out4 : std_logic_vector(7 downto 0);
-        signal data_out5 : std_logic_vector(7 downto 0);
-        signal data_out6 : std_logic_vector(7 downto 0);
+    signal clock_in : std_logic;
+	 signal paridade : std_logic;
+    signal data_in : std_logic_vector(8 downto 0);
+    signal data_out : std_logic_vector(47 downto 0);
+    signal data_out1 : std_logic_vector(7 downto 0);
+    signal data_out2 : std_logic_vector(7 downto 0);
+    signal data_out3 : std_logic_vector(7 downto 0);
+    signal data_out4 : std_logic_vector(7 downto 0);
+    signal data_out5 : std_logic_vector(7 downto 0);
+    signal data_out6 : std_logic_vector(7 downto 0);
+    signal ndata_out1 : std_logic_vector(7 downto 0);
+    signal ndata_out2 : std_logic_vector(7 downto 0);
+    signal ndata_out3 : std_logic_vector(7 downto 0);
+    signal ndata_out4 : std_logic_vector(7 downto 0);
+    signal ndata_out5 : std_logic_vector(7 downto 0);
+    signal ndata_out6 : std_logic_vector(7 downto 0);
+    component rx is
+        generic (baudrate : integer := 380);
+        port (
+            clock : in std_logic; --Entrar com o clock de 50MHz do FPGA
+            reset : in std_logic; --Reset, Lembre q o FPGA é active low, se aperta o botao o reset vai para 0
+            sin : in std_logic; --Entrada da antena
+            dado : out std_logic_vector(8 downto 0); --Saida do receptor
+            paridade : out std_logic; --Saida da paridade
+            fim : out std_logic --Sinal de controle da saida, So ler o valor de dado quando este sinal estiver em 1
+        );
+    end component;
 
-    -- componentes
-        component projeto is
-            port(
-                dado_in : in std_logic_vector(8 downto 0); --Dados de saida do RX
-                clock_in : in std_logic; --Fim do RX
-                dado_out : out std_logic_vector(47 downto 0); -- Saida com capacidade de 6 caracteres    
-                paridade: in std_logic
-            );
-        end component;
-        component rx is
-            generic (baudrate : integer := 380);
-            port (
-                clock : in std_logic; --Entrar com o clock de 50MHz do FPGA
-                reset : in std_logic; --Reset, Lembre q o FPGA é active low, se aperta o botao o reset vai para 0
-                sin : in std_logic; --Entrada da antena
-                dado : out std_logic_vector(8 downto 0); --Saida do receptor
-                paridade : out std_logic; --Saida da paridade
-                fim : out std_logic --Sinal de controle da saida, So ler o valor de dado quando este sinal estiver em 1
-            );
-        end component;
-        component display is
-            port (
-                input: in   std_logic_vector(7 downto 0); -- ASCII 8 bits
-                output: out std_logic_vector(7 downto 0)  -- ponto + abcdefg
-            );
-        end component;
-    begin 
-        get_palavra : projeto port map(data_rx, clock_projeto, data_out, paridade_rx);
-        get_caracteres : rx generic map(380) port map(clock, reset, data, data_rx, paridade_rx, clock_projeto);
-        d1 : display port map(data_out1, display1);
-        d2 : display port map(data_out2, display2);
-        d3 : display port map(data_out3, display3);
-        d4 : display port map(data_out4, display4);
-        d5 : display port map(data_out5, display5);
-        d6 : display port map(data_out6, display6);
-        data_out1 <= data_out(7 downto 0);
-        data_out2 <= data_out(15 downto 8);
-        data_out3 <= data_out(23 downto 16);
-        data_out4 <= data_out(31 downto 24);
-        data_out5 <= data_out(39 downto 32);
-        data_out6 <= data_out(47 downto 40);
-    end architecture;
+    component projeto is
+        port (
+            dado_in : in std_logic_vector(8 downto 0); --Dados de saida do RX
+            clock_in : in std_logic; --Fim do RX
+            dado_out : out std_logic_vector(47 downto 0); -- Saida com capacidade de 6 caracteres
+				paridade : in std_logic
+        );
+    end component;
+
+    component display is
+        port (
+            input : in std_logic_vector(7 downto 0); -- ASCII 8 bits
+            output : out std_logic_vector(7 downto 0) -- ponto + abcdefg
+        );
+    end component;
+
+begin
+  get_caracteres : rx generic map(380) port map(clock, reset, data, data_in, paridade, clock_in);
+  junta_caractere : projeto port map(data_in, clock_in, data_out, paridade);
+  d1 : display port map(data_out1, ndata_out1);
+  d2 : display port map(data_out2, ndata_out2);
+  d3 : display port map(data_out3, ndata_out3);
+  d4 : display port map(data_out4, ndata_out4);
+  d5 : display port map(data_out5, ndata_out5);
+  d6 : display port map(data_out6, ndata_out6);
+  data_out6 <= data_out(7 downto 0);
+  data_out5 <= data_out(15 downto 8);
+  data_out4 <= data_out(23 downto 16);
+  data_out3 <= data_out(31 downto 24);
+  data_out2 <= data_out(39 downto 32);
+  data_out1 <= data_out(47 downto 40);
+  display1 <= not(ndata_out1);
+  display2 <= not(ndata_out2);
+  display3 <= not(ndata_out3);
+  display4 <= not(ndata_out4);
+  display5 <= not(ndata_out5);
+  display6 <= not(ndata_out6);
+end architecture;
